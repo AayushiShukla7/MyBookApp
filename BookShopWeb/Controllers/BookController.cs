a49c994a634604e50d278772a06ac28328f6aae2
AayushiShukla7 <AayushiShukla27@gmail.com>
2021-04-19 02:30:03 +0530

    Controller and Model added. Startup modified to use Controller.

---
+++ BookShopWeb/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using BookShopWeb.Models;
+
+namespace BookShopWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase    //Use 'ControllerBase' instead of 'Controller' --> Makes it easier to control and use
+    {
+        public IActionResult Get()
+        {
+            var bookList = new List<Book>();
+            bookList.Add(new Book
+            {
+                Name = "The Subtle Art Of Not Giving A Fuck!",
+                PublishYear = 2016,
+                Price = 499
+            });
+            bookList.Add(new Book
+            {
+                Name = "Learn C#",
+                PublishYear = 2019,
+                Price = 123.45M
+            });
+            bookList.Add(new Book
+            {
+                Name = "Learn VueJS",
+                PublishYear = 2018,
+                Price = 45.99M
+            });
+            bookList.Add(new Book
+            {
+                Name = "Learn MongoDb",
+                PublishYear = 2019,
+                Price = 70.01M
+            });
+
+            return Ok(bookList);
+        }
+    }
+}
--- BookShopWeb/Startup.cs
+++ BookShopWeb/Startup.cs
@@ -17,6 +17,7 @@ namespace BookShopWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,7 @@ namespace BookShopWeb
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
